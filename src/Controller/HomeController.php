234dc3556dcc60b74848e73ea3d64ca80f581337
Yves Ininahazwe <yves.ininahazwe@talents-handicap.com>
2021-11-04 11:59:05 +0100

    readme

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\AnnonceRepository;
 use App\Repository\BlogRepository;
+use App\Repository\ForumRepository;
+use App\Repository\StandRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(BlogRepository $blogRepository): Response
+    public function index(BlogRepository $blogRepository, ForumRepository $forumRepository, StandRepository $standRepository): Response
     {
         $blog = $blogRepository->findAll();
+        $stand = $standRepository->findAll();
+        $forum = $forumRepository->findLastInserted();
         return $this->render('home/index.html.twig', [
             'blogs' => $blog,
+            'forums' => $forum,
+            'stands' => $stand
         ]);
     }
 
